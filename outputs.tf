5cf7a8210f2ed9862add3451cf06bcc143294a2e
Andrea Valente <33528052+kauber@users.noreply.github.com>
2025-06-21 13:28:17 +0300

    fixed outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@
 
 output "instance_public_ip" {
   description = "The public IP address of the scraper EC2 instance"
-  value       = aws_spot_instance_request.scraper_node.public_ip
+  value       = aws_instance.scraper_node.public_ip
 }
 
 output "s3_bucket_name" {
